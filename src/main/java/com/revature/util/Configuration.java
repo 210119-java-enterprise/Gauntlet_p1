c0b236e615de6fc5d31e640111dd9a94b9463842
unknown <alex.googe@revature.net>
2021-02-12 09:37:18 -0500

    Removing SessionFactory. Started it too soon. Updated Configuration.java

--- src/main/java/com/revature/util/Configuration.java
+++ src/main/java/com/revature/util/Configuration.java
@@ -1,7 +1,6 @@
 package com.revature.util;
 
 import java.io.FileReader;
-import java.io.IOException;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
@@ -11,6 +10,21 @@ public class Configuration {
 
     private List<MetaModel<Class<?>>> metaModels;
     private Properties props = new Properties();
+    private String dbUrl;
+    private String dbUser;
+    private String dbPass;
+
+    public Configuration() {
+        try {
+            props.load(new FileReader("src/main/resources/app.properties"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        dbUrl = props.getProperty("url");
+        dbUser = props.getProperty("admin-usr");
+        dbPass = props.getProperty("admin-pw");
+    }
 
     @SuppressWarnings({"unchecked"})
     public Configuration addAnnotatedClass (Class annotatedClass) {
@@ -26,19 +40,6 @@ public class Configuration {
 
     public List<MetaModel<Class<?>>> getMetaModels() {
         return (metaModels == null) ? Collections.emptyList() : metaModels;
-    }
-
-    public SessionFactory buildSessionFactory() {
-        try {
-            props.load(new FileReader("src/main/resources/app.properties"));
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
-        }
-
-        return new SessionFactory(
-                props.getProperty("url"),
-                props.getProperty("admin-usr"),
-                props.getProperty("admin-pw"));
     }
 
 }
